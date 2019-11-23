d96489ebf1660d6990c5f77e5496856246e9f27d
hedu <eperezghedu@gmail.com>
2019-11-23 18:31:59 +0100

    18 add services

---
+++ src/main/java/com/hedu/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,47 @@
+package com.hedu.spring5webapp.bootstrap;
+
+import com.hedu.spring5webapp.model.Author;
+import com.hedu.spring5webapp.model.Book;
+import com.hedu.spring5webapp.repositories.AuthorRepository;
+import com.hedu.spring5webapp.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData() {
+        //Eric
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Development", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        //Rod
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "2344", "Worx");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+}
